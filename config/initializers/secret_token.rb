44f337d38e1e0fb00ee39464449c88a6a60da3d9
Stephen Cassidy <cassidy.1113@gmail.com>
2015-04-09 13:43:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opinionated::Application.config.secret_token = 'b4ced2b0ddd820c87d7b017db6ff5775e476414d94935979b6087791fc1b2c6efc606a7282d35f9e48e3ef7077763e3c480192bdd6a2a075fd076209d4ec4206'
